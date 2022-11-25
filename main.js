c8497d1a7ceca44081941e833b5287079e66a284
Chelitis <86283476+zumosik@users.noreply.github.com>
2022-11-25 13:13:44 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function SayHello()  {
+    alert("HELLO WORLD!");
+}
+
+function SayAway() {
+    let i = 0;
+    while (i < 25) { 
+        alert("GO AWAY");
+        i++;
+    }
+}
+
+function Change() {
+    document.getElementById("test").innerHTML = "HAHAHAHAHAH" 
+}
+
+function ChangeTitle() {
+    document.title = "HAHA" 
+}
